774b2c2b3b2d9bac8efcae92f51b691cac9f142e
Rajeev <ryadav.iet01@gmail.com>
2021-04-14 15:22:59 +0530

    second commit

--- springboot-buildingblocks/springboot-buildingblocks/src/main/java/com/ry/rest/services/SpringbootBuildingblocksApplication.java
+++ springboot-buildingblocks/springboot-buildingblocks/src/main/java/com/ry/rest/services/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.ry.rest.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//first  test commit
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
