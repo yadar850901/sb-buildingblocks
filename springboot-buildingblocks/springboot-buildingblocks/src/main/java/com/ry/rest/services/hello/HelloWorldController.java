f1bdb30fd1af806e77f3c7f3a823c342d9d2c04b
Rajeev <ryadav.iet01@gmail.com>
2021-04-14 19:02:57 +0530

    missed changes

--- springboot-buildingblocks/springboot-buildingblocks/src/main/java/com/ry/rest/services/hello/HelloWorldController.java
+++ springboot-buildingblocks/springboot-buildingblocks/src/main/java/com/ry/rest/services/hello/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.ry.rest.services.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,11 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping("getUserDetails")
+	public UserDetails getUserDetails(){
+		UserDetails ob = new UserDetails("fst", "lst", "pune");
+		return ob;
 	}
 }
